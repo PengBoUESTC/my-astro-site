32fee65a873c3a9523b7251a4255a3b108b51290
pengbo43 <pengbo43@jd.com>
2022-10-26 17:59:37 +0800

    feat: add react

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   markdown: {
 
+  },
+  integrations: [react()],
+  server: {
+    host: 'jynpb.com',
+  },
+  vite: {
+    server: {
+      open: import.meta.env.MODE !== 'production'
+    }
   }
 });
